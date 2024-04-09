aa2f7a36640fc80fac6ade1259dfcfec83ef7212
Charles Meeler <cmeeler@usc.edu>
2024-04-09 17:17:45 +0000

    Wordle done but inefficient

--- wordle.cpp
+++ wordle.cpp
@@ -1,7 +1,5 @@
 #ifndef RECCHECK
-// For debugging
 #include <iostream>
-// For std::remove
 #include <algorithm> 
 #include <map>
 #include <set>
@@ -11,9 +9,8 @@
 #include "dict-eng.h"
 using namespace std;
 
-
 // Add prototypes of helper functions here
-
+void allWords(size_t remainingSize, std::set<std::string>& possibleCombos, std::string word = "");
 
 // Definition of primary wordle function
 std::set<std::string> wordle(
@@ -21,8 +18,58 @@ std::set<std::string> wordle(
     const std::string& floating,
     const std::set<std::string>& dict)
 {
-    // Add your code here
+    size_t targetSize = in.length();
+    set<string> possibleCombos;
+		set<string> realWords;
 
+    allWords(targetSize, possibleCombos);
+
+    set<string> result;
+
+    for(set<std::string>::iterator it = possibleCombos.begin(); it != possibleCombos.end(); ++it){
+        set<char> allLetters;
+        string current = *it;
+        bool requiredLetters = true, possibleLetters = true;
+
+				if(dict.find(current) == dict.end()) continue;
+
+        for(size_t i = 0; i < targetSize; i++){
+            allLetters.insert(current[i]);
+
+            if(in[i] == '-') continue;
+            else{
+                if(current[i] != in[i]){
+                    requiredLetters = false;
+                    break;
+                }
+            }
+        }
+
+        if(!requiredLetters) continue;
+
+        for(size_t i = 0; i < floating.size(); i++){
+            if(allLetters.find(floating[i]) == allLetters.end()){
+                possibleLetters = false;
+                break;
+            }
+        }
+
+        if(possibleLetters){
+            result.insert(current);
+        }
+    }
+
+    return result;
 }
 
 // Define any helper functions here
+void allWords(size_t remainingSize, std::set<std::string>& possibleCombos, std::string word){
+    if(remainingSize == 0){
+        possibleCombos.insert(word);
+        return;
+    }
+
+    for(char i = 'a'; i <= 'z'; i++){
+        allWords(remainingSize - 1, possibleCombos, word + i);
+    }
+}
