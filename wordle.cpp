f617781e2841623c8475df465d9f4cb4a40ddc16
Charles Meeler <cmeeler@usc.edu>
2024-04-14 20:47:56 +0000

    Wordle Doneish

--- wordle.cpp
+++ wordle.cpp
@@ -10,66 +10,74 @@
 using namespace std;
 
 // Add prototypes of helper functions here
-void allWords(size_t remainingSize, std::set<std::string>& possibleCombos, std::string word = "");
-
+void generatePermutations(const std::string& in, set<string>& permutations, string temp, int start, int end);
+void generateWords(const std::set<std::string>& dict, set<string>& actualWords, string current, int start, int end);
 // Definition of primary wordle function
 std::set<std::string> wordle(
     const std::string& in,
     const std::string& floating,
     const std::set<std::string>& dict)
 {
-    size_t targetSize = in.length();
-    set<string> possibleCombos;
-		set<string> realWords;
+    set<string> permutations;
+    set<string> actualWords;
+    size_t wordLength = in.length(), floatTemp = 0;
+    string temp = in;
 
-    allWords(targetSize, possibleCombos);
-
-    set<string> result;
-
-    for(set<std::string>::iterator it = possibleCombos.begin(); it != possibleCombos.end(); ++it){
-        set<char> allLetters;
-        string current = *it;
-        bool requiredLetters = true, possibleLetters = true;
-
-				if(dict.find(current) == dict.end()) continue;
-
-        for(size_t i = 0; i < targetSize; i++){
-            allLetters.insert(current[i]);
-
-            if(in[i] == '-') continue;
-            else{
-                if(current[i] != in[i]){
-                    requiredLetters = false;
-                    break;
-                }
-            }
+    for(size_t i = 0; i < wordLength; i++){
+        if(floatTemp == floating.length()){
+            break;
         }
 
-        if(!requiredLetters) continue;
-
-        for(size_t i = 0; i < floating.size(); i++){
-            if(allLetters.find(floating[i]) == allLetters.end()){
-                possibleLetters = false;
-                break;
-            }
-        }
-
-        if(possibleLetters){
-            result.insert(current);
+        else if(temp[i] == '-'){
+            temp[i] = floating[floatTemp++];
         }
     }
 
-    return result;
+    generatePermutations(in, permutations, temp, 0, wordLength);
+
+    for(set<string>::iterator it = permutations.begin(); it != permutations.end(); ++it){
+        generateWords(dict, actualWords, *it, 0, wordLength);
+    }
+
+    return actualWords;
 }
 
-// Define any helper functions here
-void allWords(size_t remainingSize, std::set<std::string>& possibleCombos, std::string word){
-    if(remainingSize == 0){
-        possibleCombos.insert(word);
+void generatePermutations(const std::string& in, set<string>& permutations, string temp, int start, int end){
+   if(start == end){
+        for(size_t i = 0; i < in.length(); i++){
+            if(in[i] != '-' && temp[i] != in[i]) return;
+        }
+        permutations.insert(temp);
         return;
     }
 
-    for(char i = 'a'; i <= 'z'; i++){
-        allWords(remainingSize - 1, possibleCombos, word + i);
+    for(int i = start; i < end; i++){
+        swap(temp[start], temp[i]);
+
+        generatePermutations(in, permutations, temp, start + 1, end);
+
+        //swap(temp[start], temp[i]);
+    }
+}
+
+void generateWords(const std::set<std::string>& dict, set<string>& actualWords, string current, int start, int end){
+    if (start == end) {
+        if (dict.find(current) != dict.end()) {
+            actualWords.insert(current);
+        }
+        return;
+    }
+
+    string temp = current;
+
+    if (temp[start] == '-') {
+        for (char i = 'a'; i <= 'z'; i++) {
+            temp[start] = i; // Replace dash with current character
+            generateWords(dict, actualWords, temp, start + 1, end); // Pass modified temp to the recursive call
+        }
+    } 
+    
+    else {
+        generateWords(dict, actualWords, temp, start + 1, end);
     }
 }
