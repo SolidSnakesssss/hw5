a839527d6c8186b578100157397b9b0fffb3c6a4
Charles Meeler <cmeeler@usc.edu>
2024-04-17 01:52:13 +0000

    Wordle mega efficient

--- wordle.cpp
+++ wordle.cpp
@@ -10,94 +10,70 @@
 using namespace std;
 
 // Add prototypes of helper functions here
-void generatePermutations(const std::string& in, set<string>& permutations, string temp, int start, int end);
-bool validPermutation(const std::string& in, string current, size_t wordLength, size_t index = 0);
-void generateWords(const std::set<std::string>& dict, set<string>& actualWords, string current, int start, int end);
+void generateWords(const string& in, string floating, const set<string>& dict, set<string>& actualWords, size_t blankCount, string current = "");
 // Definition of primary wordle function
 std::set<std::string> wordle(
     const std::string& in,
     const std::string& floating,
     const std::set<std::string>& dict)
 {
-    set<string> permutations;
     set<string> actualWords;
-    size_t wordLength = in.length(), floatTemp = 0;
-    string temp = in;
+    size_t blankCount = 0;
 
-    for(size_t i = 0; i < wordLength; i++){
-        if(floatTemp == floating.length()){
-            break;
-        } //Yorp
-
-        else if(temp[i] == '-'){
-            temp[i] = floating[floatTemp++];
+    for(size_t i = 0; i < in.size(); i++){
+        if(in[i] == '-'){
+            blankCount++;
         }
     }
 
-    generatePermutations(in, permutations, temp, 0, wordLength);
-
-    for(set<string>::iterator it = permutations.begin(); it != permutations.end(); ++it){
-        generateWords(dict, actualWords, *it, 0, wordLength);
-    }
+    generateWords(in, floating, dict, actualWords, blankCount);
 
     return actualWords;
 }
 
-void generatePermutations(const std::string& in, set<string>& permutations, string temp, int start, int end){
-   if(start == end){
-        if(validPermutation(in, temp, in.length())){
-            permutations.insert(temp);
-        }
-        
+void generateWords(const string& in, string floating, const set<string>& dict, set<string>& actualWords, size_t blankCount, string current){
+    //Are there less blank spaces than there are floating characters
+		if(blankCount < floating.size()){
         return;
     }
 
-    for(int i = start; i < end; i++){
-        swap(temp[start], temp[i]);
-
-        generatePermutations(in, permutations, temp, start + 1, end);
-
-        //swap(temp[start], temp[i]);
-    }
-}
-
-//Helper function for checking if the permutation is valid
-bool validPermutation(const std::string& in, string current, size_t wordLength, size_t index){
-    
-		//Returns true if in chars are in the right place
-		if(index == wordLength){
-        return true;
-    }
-
-		//Returns false if in chars are in the right place
-    else if(in[index] != '-' && current[index] != in[index]){
-        return false;
-    }
-
-    return validPermutation(in, current, wordLength, index + 1);
-}
-
-void generateWords(const std::set<std::string>& dict, set<string>& actualWords, string current, int start, int end){
-    if (start == end) {
-        
-				//Best Version but requires find
-        if (dict.find(current) != dict.end()) {
-            actualWords.insert(current);
+		//Checks if the current words is of size n
+    else if(current.size() == in.size()){
+				//Checks if floating is empty and that the word is in the dictionary
+        if(dict.find(current) != dict.end() && floating.empty()){
+            //Adds the word to actualWords if found
+						actualWords.insert(current);
         }
-
-        return;
     }
 
-    string temp = current;
+		//Checks if there is a green letter at index[current.size()]
+    else if(in[current.size()] != '-'){
+        //Adds the green letter to current
+				generateWords(in, floating, dict, actualWords, blankCount, current + in[current.size()]);
+    }
 
-    if (temp[start] == '-') {
-        for (char i = 'a'; i <= 'z'; i++) {
-            temp[start] = i; // Replace dash with current character
-            generateWords(dict, actualWords, temp, start + 1, end); // Pass modified temp to the recursive call
+		//Searches the whole alphabet if spot is blank
+    else{
+				//Subtract because blank space is being filled
+        blankCount--;
+
+				//Loops thorugh the whole alphabet
+        for(char i = 'a'; i <= 'z'; i++){
+            //Looks to see if the current char is in floating
+						size_t floatingIndex = floating.find(i);
+            
+						//Removes the letter from floating if found. Only removes 1 in case of duplicates
+            if(floatingIndex != string::npos){
+                floating.erase(floatingIndex, 1);
+                generateWords(in, floating, dict, actualWords, blankCount, current + i);
+                //Adds the char back for future iterations of the loop
+								floating += i;
+            }
+
+						//Does nothing to floating if char is not in floating
+            else{
+                generateWords(in, floating, dict, actualWords, blankCount, current + i);
+            }
         }
-    } 
-    
-    else {
-        generateWords(dict, actualWords, temp, start + 1, end);
     }
 }
